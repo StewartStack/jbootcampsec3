a7d30041ae6036061309cafca89f8120b8f7b71e
John Stewart <jstewartstack@gmail.com>
2023-01-06 18:11:56 +0000

    equals and not equals

--- BooleansComparisons.java
+++ BooleansComparisons.java
@@ -5,10 +5,13 @@ public class BooleansComparisons {
           int biologyGrade = 75;
           int englishGrade = 75;
           
-          System.out.println(chemistryGrade == englishGrade);
-          System.out.println(biologyGrade == englishGrade);
+          System.out.println(chemistryGrade != englishGrade);
+          System.out.println(biologyGrade != englishGrade);
 
+          String sentence = "I love this course!";
+          String sentence2 = "I love this course!";
 
-
+          System.out.println(sentence.equals(sentence2));
+          System.out.println(!sentence.equals(sentence2));
      }
 }
