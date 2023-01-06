84717e958a23d9f1d553fc7cdd77b39293bbea77
John Stewart <jstewartstack@gmail.com>
2023-01-06 18:08:13 +0000

    equalorless

--- BooleansComparisons.java
+++ BooleansComparisons.java
@@ -3,13 +3,11 @@ public class BooleansComparisons {
      public static void main(String[] args) {
           int chemistryGrade = 95;
           int biologyGrade = 75;
+          int englishGrade = 75;
+          
+          System.out.println(chemistryGrade <= englishGrade);
+          System.out.println(biologyGrade <= englishGrade);
 
-          System.out.println(biologyGrade < chemistryGrade);
-
-          double sales = 37.55;
-          double costs = 5.55;
-
-          System.out.println(sales < costs);
 
 
      }
