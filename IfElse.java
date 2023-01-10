078be31120de1274361c0fb0815b8def32508110
John Stewart <jstewartstack@gmail.com>
2023-01-10 20:38:27 +0000

    false code

--- IfElse.java
+++ IfElse.java
@@ -2,7 +2,7 @@ public class IfElse {
      
      public static void main(String[] args) {
           
-          int grade = 65;
+          int grade = 45;
 
           if (grade > 50) {
                System.out.println("Great job, you passed!");
