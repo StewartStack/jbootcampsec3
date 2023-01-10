b815106cd618bd3172b77dfb9e5c89b0398769ce
John Stewart <jstewartstack@gmail.com>
2023-01-10 20:50:27 +0000

    code written and compiled

---
+++ Workbook3.2/JavaMart.java
@@ -0,0 +1,27 @@
+public class JavaMart {
+    public static void main(String[] args) {
+        double wallet = 100;
+
+        //See Learn the Part for the complete instructions (link in resources folder of Udemy video).  
+
+ 
+        double toyCar = 5.99;
+        System.out.println("Can I get this car?");
+       
+    
+        if (toyCar <= wallet) {
+            System.out.println("sure!");
+        } else {
+            System.out.println("Sorry, I only have " + wallet + " left.");
+        }
+        
+        double nike = 95.99;
+        System.out.println("Can I get these nike shoes?");
+        
+        if (nike <= wallet) {
+            System.out.println("Sure");
+        } else {
+            System.out.println("Sorry, I only have " + wallet + " left.");
+        }
+    }
+}
