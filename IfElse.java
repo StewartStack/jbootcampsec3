60a37f70a968fc189bd272a0f89fdd306bb82837
John Stewart <jstewartstack@gmail.com>
2023-01-10 20:37:14 +0000

    fixed

---
+++ IfElse.java
@@ -0,0 +1,12 @@
+public class IfElse {
+     
+     public static void main(String[] args) {
+          
+          int grade = 65;
+
+          if (grade > 50) {
+               System.out.println("Great job, you passed!");
+          }
+     }
+
+}
