8028e695625270f05e1a1c70df2210a0725b7e16
John Stewart <jstewartstack@gmail.com>
2023-02-02 09:56:27 +0000

    finished

--- IfElse.java
+++ IfElse.java
@@ -2,13 +2,21 @@ public class IfElse {
      
      public static void main(String[] args) {
           
-          int grade = 50;
+          int grade = 65;
 
-          if (grade >= 50) {
-               System.out.println("Great job, you passed!");
+          if (grade >= 80) {
+               System.out.println("You got an A!"); //false
+          } else if (grade >= 70) {
+               System.out.println("you got a B!"); //false
+          } else if (grade >= 60) {
+               System.out.println("you got a C!"); //true
+          } else if (grade >= 50) {
+               System.out.println("you got a D!"); //ignored as condition met
           } else {
-               System.out.println("Sorry, unfortunately you did not pass!");
+               System.out.println("you got a F!"); // ignored as condition met
           }
      }
-
 }
+     
+
+
