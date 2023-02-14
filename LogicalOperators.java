7448dc04da7a8a28c2634d3906233f9735b260d9
John Stewart <jstewartstack@gmail.com>
2023-02-14 14:18:24 +0000

    finished

--- LogicalOperators.java
+++ LogicalOperators.java
@@ -1,5 +1,23 @@
 public class LogicalOperators {
     public static void main(String[] args) {
+          int chemistryGrade = 67;
+          int englishGrade = 65;
+          String language = "Java";
+
+          if (chemistryGrade > 75 || englishGrade > 75 || language.equals("Java")) {
+               System.out.println("Congrats, you passsed!");
+          } else {
+               System.out.println("You didn't pass!");
+          }
           
+          int credits = 56; // both or these must be right to pass
+          double GPA = 1.8;
+
+          if (credits >= 40 && GPA >= 2.0) {
+               System.out.println("You earned diploma!");
+          } else {
+               System.out.println("You didn't earn diploma!");
+          }
     } 
+
 }
