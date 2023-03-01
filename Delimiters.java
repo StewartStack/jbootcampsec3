2a8038066629e5f6124bbfb0c33965767056d68c
John Stewart <jstewartstack@gmail.com>
2023-03-01 16:52:27 +0000

    finished

---
+++ Delimiters.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class Delimiters {
+     public static void main(String[] args) {
+          Scanner scan = new Scanner(System.in);
+          
+          System.out.println("Please enter a number");
+          //int num2 = scan.nextInt();
+          int num = scan.nextInt();
+          
+          scan.nextLine();
+          
+          System.out.println("Please write a sentence");
+          String sentence = scan.nextLine();
+
+          
+          /*
+          System.out.println("Enter two integers (on the same line)");
+          int num1 = scan.nextInt();
+          int num2 = scan.nextInt();
+          
+          System.out.println("Enter two very big integers (on the same line)");
+          long bigNum1 = scan.nextLong();
+          long bigNum2 = scan.nextLong();
+          
+          System.out.println("Enter two decimals (on the same line)");
+          double decimal1 = scan.nextDouble();
+          double decimal2 = scan.nextDouble();
+          
+          System.out.println("Enter two text values (on the same line)");
+          String text1 = scan.next();
+          String text2 = scan.next();
+
+          
+
+          System.out.println("\tIntegers: " + num1 + " " + num2);
+          System.out.println("\tBig Integers: " + bigNum1 + " " + bigNum2);
+          System.out.println("\tDecimals: " + decimal1 + " " + decimal2);
+          System.out.println("\tString: " + text1 + " " + text2);
+          */
+
+          scan.close();
+     }
+}
